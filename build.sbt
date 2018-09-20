e25f4071956e5af449027d0cb7b92972ffd2a5f9
ragnar <ragnar@rmgk.de>
2018-09-20 17:01:23 +0200

    error handling

--- build.sbt
+++ build.sbt
@@ -6,20 +6,25 @@ enablePlugins(JavaAppPackaging)
 
 name := "tfl"
 
+val monocleVersion = "1.5.0-cats" // 1.5.0-cats based on cats 1.0.x
+
 libraryDependencies ++= Seq(
   "com.monovore" %% "decline" % "0.5.0",
   "com.github.pathikrit" %% "better-files" % "3.6.0",
   "com.lihaoyi" %% "fastparse" % "1.0.0",
   "org.scalatest" %% "scalatest" % "3.0.5" % "test",
   "org.scalacheck" %% "scalacheck" % "1.14.0" % "test",
-  "com.lihaoyi" %% "pprint" % "0.5.3"
-)
+  "com.lihaoyi" %% "pprint" % "0.5.3",
+  "com.github.julien-truffaut" %% "monocle-core" % monocleVersion,
+  "com.github.julien-truffaut" %% "monocle-macro" % monocleVersion,
+  )
 
 
 
 Compile / compile / scalacOptions ++= Seq(
   "-deprecation",
   "-encoding", "UTF-8",
+  "-Ypatmat-exhaust-depth", "65536",
   "-unchecked",
   "-feature",
   "-Xlint",
--- src/main/scala/de/rmgk/tfl/Tfl.scala
+++ src/main/scala/de/rmgk/tfl/Tfl.scala
@@ -11,7 +11,8 @@ object Tfl {
     def format(): String = toString
     def tex(): String = toString
   }
-  sealed trait Value extends Term
+  sealed trait Result extends Term
+  sealed trait Value extends Result
 
   case class Identifier(scala: Symbol) extends Term {
     override def format(): String = scala.name
@@ -30,6 +31,9 @@ object Tfl {
   case class Text(str: String) extends Value
   implicit def stringToText(str: String): Text = Text(str)
 
+  case class Error(msg: String) extends Result
+  case class TryCatch(body: Term, handler: Term) extends Term
+
 
   type Environment = Map[Identifier, Value]
 
@@ -41,59 +45,63 @@ object Tfl {
   def id: Fun = 'x =>: 'x
   def zero: Fun = 'f =>: 'x =>: 'x
   def one: Fun = id
-  def succ: Fun = 'a =>: 'f =>: 'x =>: 'f('a('f)('x))
-  def add: Fun = 'a =>: 'b =>: 'f =>: 'x =>: 'a('f)('b('f)('x))
+  def succ: Fun = 'a =>: 'f =>: 'x =>: 'f ('a ('f)('x))
+  def add: Fun = 'a =>: 'b =>: 'f =>: 'x =>: 'a ('f)('b ('f)('x))
   def two: App = add(one)(one)
-  def times: Fun = 'a =>: 'b =>: 'f =>: 'x =>: 'a('b('f)('x))
+  def times: Fun = 'a =>: 'b =>: 'f =>: 'x =>: 'a ('b ('f)('x))
 
   def freeVariables(term: Term): Set[Identifier] = term match {
-    case id: Identifier => Set(id)
-    case Fun(parameter, body) => freeVariables(body) - parameter
+    case id: Identifier          => Set(id)
+    case Fun(parameter, body)    => freeVariables(body) - parameter
     case App(function, argument) => freeVariables(function) ++ freeVariables(argument)
-    case _ : Text => Set()
+    case TryCatch(body, handler) => freeVariables(body) ++ freeVariables(handler)
+    case _: Text | _: Error      => Set()
   }
 
 
-  def interpret(term: Term, environment: Environment): Value = {
+  def interpret(term: Term, environment: Environment): Result = {
     term match {
-      case id: Identifier                  => environment(id)
+      case id: Identifier => environment(id)
 //      case fun: Fun                        =>
 //        val environmentOfFreeVariables = environment -- (environment.keySet -- freeVariables(fun))
 //        Closure(fun, environmentOfFreeVariables)
       case App(functionTerm, argumentTerm) =>
         val argument = interpret(argumentTerm, environment)
         interpret(functionTerm, environment) match {
-          case function : Fun =>
+          case function: Fun =>
             interpret(subs(function.body, function.parameter, argument), Map())
-          case t @ Text(s1) =>
+          case t@Text(s1)    =>
             argument match {
               case Text(s2) => Text(s1 + s2)
-              case _ => 'x =>: t(argument('x))
+              case _        => 'x =>: t(argument('x))
             }
+          case other: Error  => other
 
         }
-      case v: Value                        => v
+      case TryCatch(body, handler)         => interpret(body, environment) match {
+        case _: Error => interpret(handler, environment)
+        case other    => other
+      }
+      case v: Result                       => v
     }
   }
 
-  def subs(term: Term, parameter: Identifier, argument: Value): Term = {
+  def subs(term: Term, parameter: Identifier, argument: Term): Term = {
     val freeVar = freeVariables(argument)
     assert(freeVar == Set(), s"unbound identifier $freeVar")
     term match {
-      case id: Identifier                  if id == parameter => argument
-      case Fun(param, body)         if param != parameter    =>
+      case id: Identifier if id == parameter      => argument
+      case Fun(param, body) if param != parameter =>
         Fun(param, subs(body, parameter, argument))
-      case App(functionTerm, argumentTerm) =>
+      case App(functionTerm, argumentTerm)        =>
         App(subs(functionTerm, parameter, argument), subs(argumentTerm, parameter, argument))
-//      case Closure(fun, env)               =>
-//        val senv = env.map { case (k, v) => k -> interpret(subs(v, Map()), Map()) }
-//        subs(fun, senv)
-      case other                      => other
+      case other                                  => other
     }
   }
 
   case class Configuration(term: Term) {
-    def derive(name: String, to: Configuration, premises: List[Rule] = Nil): Some[Rule] = Some(Rule(name, this, to, premises))
+    def derive(name: String, to: Term, premises: List[Rule] = Nil): Some[Rule] =
+      Some(Rule(name, this, this.copy(to), premises))
     def format(): String = term.format()
     def tex(): String = term.tex()
   }
@@ -106,34 +114,43 @@ object Tfl {
   }
 
   def step(conf: Configuration): Option[Rule] = {
+
+    def context(term: Term, outer: Term => Term): Option[Rule] = {
+      term match {
+        case err: Error => conf.derive("error", err)
+        case nonError   =>
+          step(conf.copy(nonError)).flatMap { inner =>
+          conf.derive("context", outer(inner.to.term), List(inner))
+        }
+      }
+
+    }
+
     conf.term match {
-      case _: Value                        => None
-      case id: Identifier                  => None
-      case App(function: Fun, argument: Value) => conf.derive("app", {
-        Configuration(subs(function.body, function.parameter, argument))
-      })
-      case App(Text(s1), Text(s2)) => conf.derive("concat text", {
-        conf.copy(Text(s1 + s2))
-      })
-      case App(t@Text(s1), v: Fun) => conf.derive("concat other", {
-        conf.copy('x =>: t(v('x)))
-      })
-      case app @ App(closure: Value, argument) =>
-        step(conf.copy(argument)).flatMap{ inner =>
-          conf.derive("context", conf.copy(app.copy(argument = inner.to.term)), List(inner))
-        }
-      case app @ App(function, _) =>
-        step(conf.copy(function)).flatMap { inner =>
-          conf.derive("context", conf.copy(app.copy(inner.to.term)), List(inner))
-        }
+      case _: Result      => None
+      case id: Identifier => None
+
+      case App(function: Fun, argument: Value) => conf.derive("app",
+                                                              subs(function.body, function.parameter, argument))
+      case App(Text(s1), Text(s2))             => conf.derive("concat text", Text(s1 + s2))
+      case App(t@Text(s1), v: Fun)             => conf.derive("concat other", 'x =>: t(v('x)))
+
+      case TryCatch(Error(_), handler) => conf.derive("catch", handler)
+      case TryCatch(value: Value, _)   => conf.derive("try", value)
+
+      case tc@TryCatch(body, _)        => context(body, inner => tc.copy(body = inner))
+      case app@App(_: Value, argument) => context(argument, inner => app.copy(argument = inner))
+      case app@App(function, _)        => context(function, inner => app.copy(function = inner))
+
     }
   }
 
+  @scala.annotation.tailrec
   def stepAll(conf: Configuration): Configuration = {
     step(conf) match {
-      case None => conf
+      case None    => conf
       case Some(p) =>
-        println(p.tex)
+        println(p.format())
         //pprintln(p, height = 500)
         stepAll(p.to)
     }
@@ -141,21 +158,22 @@ object Tfl {
 
   def toInt(value: Term): Option[Int] = {
 
-
+    @scala.annotation.tailrec
     def evaluateToText(t: Term): Text = t match {
       case txt: Text => txt
       case _         =>
         evaluateToText(interpret(t(""), Map()))
     }
+
     val res = evaluateToText(value("1"))
     res match {
       case Text(str) => Some(str.count(_ == '1'))
-      case _ => None
+      case _         => None
     }
   }
 
   def main(args: Array[String]): Unit = {
-    val program = ('x =>: 'x('x)('x)('x))('y =>: 'z =>:  'z('y))
+    val program = TryCatch(one(Error("oh noes")), two)
 //    val program = times(add(one)(two))(one(add(succ(two))(times(two)(succ(two)))))
     pprintln(toInt(program))
 //    val res = interpret(program, Map())
